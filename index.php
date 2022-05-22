fe10471bffca8146f26a07bd1d89c2be30342ffa
vicente romero <vbromero@miuandes.cl>
2022-05-21 23:27:14 -0400

    Anashei3

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-success">
-  <a class="navbar-brand nav" href="#">Docufácil</a>
+  <a class="navbar-brand nav" href="http://localhost/docufacil/index.php">Docufácil</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- info.php
+++ info.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-success">
-  <a class="navbar-brand" href="#">Docufácil</a>
+  <a class="navbar-brand nav" href="http://localhost/docufacil/index.php">Docufácil</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -19,13 +19,13 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="http://localhost/docufacil/index.php">Inicio <span class="sr-only">(current)</span></a>
+        <a class="nav-link nav" href="http://localhost/docufacil/index.php">Inicio <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#contacto">Contacto</a>
+        <a class="nav-link nav" href="#contacto">Contacto</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://localhost/docufacil/info.php">Acerca de Nosotros</a>
+        <a class="nav-link nav" href="http://localhost/docufacil/info.php">Acerca de Nosotros</a>
       </li>
     </ul>
   </div>
@@ -34,7 +34,7 @@
 
     <a id="acerca">
       <div>
-          <img src="images/work.jpg" class="img-fluid">
+          <img src="images/work.png" class="img-fluid center-img">
       </div>
     </a>
 
